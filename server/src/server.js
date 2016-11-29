1f31652726716b7ccf12b982d28d3c5e4c5d0a87
William Jeong <wjeong@umass.edu>
2016-11-29 16:28:02 -0500

    express

---
+++ server/src/server.js
@@ -0,0 +1,31 @@
+var Util = require('./util.js');
+var reverseString = Util.reverseString;
+var bodyParser = require('body-parser');
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+// Defines what happens when it receives the `GET /` request
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+app.use(bodyParser.text());
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+  // How do we get the input text?
+  // How do we send the output text?
+  // If the request came with text, then the text() middleware handled it
+  // and made `req.body` a string.
+  // Check that req.body is a string.
+  if (typeof(req.body) === 'string') {
+    var reversed = reverseString(req.body);
+    res.send(reversed);
+  } else {
+    // POST did not contain a string. Send an error code back!
+    res.status(400).end();
+  }
+});
