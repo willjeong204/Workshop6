d6153ea40ba223c35701426e5bdfff2db97e9b73
William Jeong <wjeong@umass.edu>
2016-11-29 18:52:46 -0500

    getFeedData

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
-var Util = require('./util.js');
-var reverseString = Util.reverseString;
+var database = require('./database.js')
+var readDocument = database.readDocument;
+//var Util = require('./util.js');
+//var reverseString = Util.reverseString;
 var bodyParser = require('body-parser');
 // Imports the express Node module.
 var express = require('express');
@@ -15,3 +17,76 @@ app.use(bodyParser.text());
 // '..' means "go up one directory", so this translates into `client/build`!
 app.use(express.static('../client/build'));
 // Handle POST /reverse [data]
+/**
+* Resolves a feed item. Internal to the server, since it's synchronous.
+*/
+function getFeedItemSync(feedItemId) {
+  var feedItem = readDocument('feedItems', feedItemId);
+  // Resolve 'like' counter.
+  feedItem.likeCounter = feedItem.likeCounter.map((id) =>
+  readDocument('users', id));
+  // Assuming a StatusUpdate. If we had other types of
+  // FeedItems in the DB, we would
+  // need to check the type and have logic for each type.
+  feedItem.contents.author = readDocument('users',
+  feedItem.contents.author);
+  // Resolve comment author.
+  feedItem.comments.forEach((comment) => {
+    comment.author = readDocument('users', comment.author);
+  });
+  return feedItem;
+}
+/**
+* Get the feed data for a particular user.
+*/
+function getFeedData(user) {
+  var userData = readDocument('users', user);
+  var feedData = readDocument('feeds', userData.feed);
+  // While map takes a callback, it is synchronous,
+  // not asynchronous. It calls the callback immediately.
+  feedData.contents = feedData.contents.map(getFeedItemSync);
+  // Return FeedData with resolved references.
+  return feedData;
+}
+/**
+* Get the feed data for a particular user.
+*/
+app.get('/user/:userid/feed', function(req, res) {
+  var userid = req.params.userid;
+  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // userid is a string. We need it to be a number.
+  // Parameters are always strings.
+  var useridNumber = parseInt(userid, 10);
+  if (fromUser === useridNumber) {
+    // Send response.
+    res.send(getFeedData(userid));
+  } else {
+    // 401: Unauthorized request.
+    res.status(401).end();
+  }
+});
+/**
+* Get the user ID from a token. Returns -1 (an invalid ID)
+* if it fails.
+*/
+function getUserIdFromToken(authorizationLine) {
+  try {
+    // Cut off "Bearer " from the header value.
+    var token = authorizationLine.slice(7);
+    // Convert the base64 string to a UTF-8 string.
+    var regularString = new Buffer(token, 'base64').toString('utf8');
+    // Convert the UTF-8 string into a JavaScript object.
+    var tokenObj = JSON.parse(regularString);
+    var id = tokenObj['id'];
+    // Check that id is a number.
+    if (typeof id === 'number') {
+      return id;
+    } else {
+      // Not a number. Return -1, an invalid ID.
+      return -1;
+    }
+  } catch (e) {
+    // Return an invalid ID.
+    return -1;
+  }
+}
