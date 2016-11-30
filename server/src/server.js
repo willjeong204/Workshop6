e8e95bb2104034851b724952a687f76ff5084679
William Jeong <wjeong@umass.edu>
2016-11-30 16:55:30 -0500

    almostDone

--- server/src/server.js
+++ server/src/server.js
@@ -132,39 +132,176 @@ return newStatusUpdate;
 }
 // `POST /feeditem { userId: user, location: location, contents: contents }`
 app.post('/feeditem',
-validate({ body: StatusUpdateSchema }), function(req, res) {
-// If this function runs, `req.body` passed JSON validation!
-var body = req.body;
+  validate({ body: StatusUpdateSchema }), function(req, res) {
+    // If this function runs, `req.body` passed JSON validation!
+    var body = req.body;
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    // Check if requester is authorized to post this status update.
+    // (The requester must be the author of the update.)
+    if (fromUser === body.userId) {
+      var newUpdate = postStatusUpdate(body.userId, body.location,
+        body.contents);
+        // When POST creates a new resource, we should tell the client about it
+        // in the 'Location' header and use status code 201.
+        res.status(201);
+        res.set('Location', '/feeditem/' + newUpdate._id);
+        // Send the update!
+        res.send(newUpdate);
+      } else {
+        // 401: Unauthorized.
+        res.status(401).end();
+      }
+});
+/**
+* Translate JSON Schema Validation failures into error 400s.
+*/
+app.use(function(err, req, res, next) {
+  if (err.name === 'JsonSchemaValidation') {
+    // Set a bad request http response status
+    res.status(400).end();
+  } else {
+    // It's some other sort of error; pass it to next error middleware handler
+    next(err);
+  }
+});
+// Reset database.
+app.post('/resetdb', function(req, res) {
+  console.log("Resetting database...");
+  // This is a debug route, so don't do any validation.
+  database.resetDatabase();
+  // res.send() sends an empty response with status code 200
+  res.send();
+});
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+// Update a feed item.
+app.put('/feeditem/:feeditemid/content', function(req, res) {
 var fromUser = getUserIdFromToken(req.get('Authorization'));
-// Check if requester is authorized to post this status update.
-// (The requester must be the author of the update.)
-if (fromUser === body.userId) {
-var newUpdate = postStatusUpdate(body.userId, body.location,
-body.contents);
-// When POST creates a new resource, we should tell the client about it
-// in the 'Location' header and use status code 201.
-res.status(201);
-res.set('Location', '/feeditem/' + newUpdate._id);
-// Send the update!
-res.send(newUpdate);
+var feedItemId = req.params.feeditemid;
+var feedItem = readDocument('feedItems', feedItemId);
+// Check that the requester is the author of this feed item.
+if (fromUser === feedItem.contents.author) {
+// Check that the body is a string, and not something like a JSON object.
+// We can't use JSON validation here, since the body is simply text!
+if (typeof(req.body) !== 'string') {
+// 400: Bad request.
+res.status(400).end();
+return;
+}
+// Update text content of update.
+feedItem.contents.contents = req.body;
+writeDocument('feedItems', feedItem);
+res.send(getFeedItemSync(feedItemId));
 } else {
 // 401: Unauthorized.
 res.status(401).end();
 }
 });
 /**
-* Translate JSON Schema Validation failures into error 400s.
+* Delete a feed item.
 */
-app.use(function(err, req, res, next) {
-if (err.name === 'JsonSchemaValidation') {
-// Set a bad request http response status
-res.status(400).end();
-} else {
-// It's some other sort of error; pass it to next error middleware handler
-next(err);
+app.delete('/feeditem/:feeditemid', function(req, res) {
+var fromUser = getUserIdFromToken(req.get('Authorization'));
+// Convert from a string into a number.
+var feedItemId = parseInt(req.params.feeditemid, 10);
+var feedItem = readDocument('feedItems', feedItemId);
+// Check that the author of the post is requesting the delete.
+if (feedItem.contents.author === fromUser) {
+database.deleteDocument('feedItems', feedItemId);
+// Remove references to this feed item from all other feeds.
+var feeds = database.getCollection('feeds');
+var feedIds = Object.keys(feeds);
+feedIds.forEach((feedId) => {
+var feed = feeds[feedId];
+var itemIdx = feed.contents.indexOf(feedItemId);
+if (itemIdx !== -1) {
+// Splice out of array.
+feed.contents.splice(itemIdx, 1);
+// Update feed.
+database.writeDocument('feeds', feed);
 }
 });
-// Starts the server on port 3000!
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+// Send a blank response to indicate success.
+res.send();
+} else {
+// 401: Unauthorized.
+res.status(401).end();
+}
+});
+// Like a feed item.
+app.put('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+var fromUser = getUserIdFromToken(req.get('Authorization'));
+// Convert params from string to number.
+var feedItemId = parseInt(req.params.feeditemid, 10);
+var userId = parseInt(req.params.userid, 10);
+if (fromUser === userId) {
+var feedItem = readDocument('feedItems', feedItemId);
+// Add to likeCounter if not already present.
+if (feedItem.likeCounter.indexOf(userId) === -1) {
+feedItem.likeCounter.push(userId);
+writeDocument('feedItems', feedItem);
+}
+// Return a resolved version of the likeCounter
+res.send(feedItem.likeCounter.map((userId) =>
+readDocument('users', userId)));
+} else {
+// 401: Unauthorized.
+res.status(401).end();
+}
+});
+// Unlike a feed item.
+app.delete('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+var fromUser = getUserIdFromToken(req.get('Authorization'));
+// Convert params from string to number.
+var feedItemId = parseInt(req.params.feeditemid, 10);
+var userId = parseInt(req.params.userid, 10);
+if (fromUser === userId) {
+var feedItem = readDocument('feedItems', feedItemId);
+var likeIndex = feedItem.likeCounter.indexOf(userId);
+// Remove from likeCounter if present
+if (likeIndex !== -1) {
+feedItem.likeCounter.splice(likeIndex, 1);
+writeDocument('feedItems', feedItem);
+}
+// Return a resolved version of the likeCounter
+// Note that this request succeeds even if the
+// user already unliked the request!
+res.send(feedItem.likeCounter.map((userId) =>
+readDocument('users', userId)));
+} else {
+// 401: Unauthorized.
+res.status(401).end();
+}
+});
+// Search for feed item
+app.post('/search', function(req, res) {
+var fromUser = getUserIdFromToken(req.get('Authorization'));
+var user = readDocument('users', fromUser);
+if (typeof(req.body) === 'string') {
+// trim() removes whitespace before and after the query.
+// toLowerCase() makes the query lowercase.
+var queryText = req.body.trim().toLowerCase();
+// Search the user's feed.
+var feedItemIDs = readDocument('feeds', user.feed).contents;
+// "filter" is like "map" in that it is a magic method for
+// arrays. It takes an anonymous function, which it calls
+// with each item in the array. If that function returns 'true',
+// it will include the item in a return array. Otherwise, it will
+// not.
+// Here, we use filter to return only feedItems that contain the
+// query text.
+// Since the array contains feed item IDs, we later map the filtered
+// IDs to actual feed item objects.
+res.send(feedItemIDs.filter((feedItemID) => {
+var feedItem = readDocument('feedItems', feedItemID);
+return feedItem.contents.contents
+.toLowerCase()
+.indexOf(queryText) !== -1;
+}).map(getFeedItemSync));
+} else {
+// 400: Bad Request.
+res.status(400).end();
+}
 });
